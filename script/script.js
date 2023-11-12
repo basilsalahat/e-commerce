c865513e1bcd20910d69c1c0aa01c867c957752c
Basil Salahat <112906018+basilsalahat@users.noreply.github.com>
2023-11-12 13:34:52 +0200

    Finish the navbar

--- script/script.js
+++ script/script.js
@@ -1,1 +1,30 @@
-console.log("Done!")
+function showSideMenu(){
+    var sideMenu = document.getElementById("nav__sideMenu");
+    sideMenu.classList.add("active");
+}
+function hideSideMenu(){
+    var sideMenu = document.getElementById("nav__sideMenu");
+    sideMenu.classList.remove("active");
+}
+
+function showCartMenu(){
+    var cartMenu = document.getElementById("nav__cartMenu");
+    cartMenu.classList.add("active");
+}
+function hideCartMenu(){
+    var cartMenu = document.getElementById("nav__cartMenu");
+    cartMenu.classList.remove("active");
+}
+
+window.addEventListener("resize", (
+    function hideMenuOnResize(){
+        if (window.innerWidth >= 992){
+            hideSideMenu();
+        }
+    }
+));
+
+function showCartItems(){
+    var cartItems = document.getElementById("nav__cartItems");
+    cartItems.classList.remove("active");
+}
