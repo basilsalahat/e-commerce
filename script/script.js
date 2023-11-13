db030ab23c225d059f1566eefca13994c7a243fb
Basil Salahat <112906018+basilsalahat@users.noreply.github.com>
2023-11-13 09:28:30 +0200

    navbar required changes

--- script/script.js
+++ script/script.js
@@ -1,30 +1,15 @@
-function showSideMenu(){
-    var sideMenu = document.getElementById("nav__sideMenu");
-    sideMenu.classList.add("active");
-}
-function hideSideMenu(){
-    var sideMenu = document.getElementById("nav__sideMenu");
-    sideMenu.classList.remove("active");
+function sideMenu() {
+  var sideMenu = document.getElementById("side-menu");
+  sideMenu.classList.toggle("active");
 }
 
-function showCartMenu(){
-    var cartMenu = document.getElementById("nav__cartMenu");
-    cartMenu.classList.add("active");
-}
-function hideCartMenu(){
-    var cartMenu = document.getElementById("nav__cartMenu");
-    cartMenu.classList.remove("active");
+function cartMenu() {
+  var cartMenu = document.getElementById("nav__cartMenu");
+  cartMenu.classList.toggle("active");
 }
 
-window.addEventListener("resize", (
-    function hideMenuOnResize(){
-        if (window.innerWidth >= 992){
-            hideSideMenu();
-        }
-    }
-));
-
-function showCartItems(){
-    var cartItems = document.getElementById("nav__cartItems");
-    cartItems.classList.remove("active");
-}
+window.addEventListener("resize", function hideMenuOnResize() {
+  if (window.innerWidth >= 992) {
+    document.getElementById("side-menu").classList.remove("active");
+  }
+});
