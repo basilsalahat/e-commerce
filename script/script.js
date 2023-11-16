8d10f15d065b7f45e7ce0470bd45537f11b740e0
Basil Salahat <112906018+basilsalahat@users.noreply.github.com>
2023-11-16 13:36:26 +0200

    Make cart item dynamic

--- script/script.js
+++ script/script.js
@@ -16,13 +16,20 @@ window.addEventListener("resize", function hideMenuOnResize() {
 
 function increseCount() {
   var count = document.getElementById("product-count");
+  var addToCartButton = document.getElementById("add-to-cart");
   count.innerHTML = parseInt(count.innerHTML) + 1;
+  addToCartButton.classList.remove("disabled");
 }
 
 function decreseCount() {
   var count = document.getElementById("product-count");
+  console.log(count.innerHTML);
   if (parseInt(count.innerHTML) > 0) {
     count.innerHTML = parseInt(count.innerHTML) - 1;
+    if (parseInt(count.innerHTML) == 0) {
+      var addToCartButton = document.getElementById("add-to-cart");
+      addToCartButton.classList.add("disabled");
+    }
   }
 }
 
@@ -74,4 +81,65 @@ function changeExpandedImg(img) {
   var expandImg = document.getElementById("product-expanded-image");
   console.log(expandImg);
   expandImg.src = img.src;
+}
+
+// Fill items if exists in cart items section when load the page
+
+var items = [];
+var itemsConverted = JSON.stringify(items);
+localStorage.setItem("cart-items", itemsConverted);
+window.addEventListener("load", fillCartItems());
+
+function fillCartItems() {
+  var itemsFromStorage = JSON.parse(localStorage.getItem("cart-items"));
+  if (itemsFromStorage != null) {
+    for (var i = 0; i < itemsFromStorage.length; i++) {
+      console.log(itemsFromStorage);
+    }
+  }
+}
+
+function addCurrentItemToCart() {
+  var cartItemsCountainer = document.getElementsByClassName("nav__items");
+  var checkOutButton = document.getElementsByClassName("nav__checkOutButton");
+  cartItemsCountainer[0].classList.remove("empty");
+  checkOutButton[0].classList.remove("disabled");
+
+  //  get the prooduct data
+  var productID = document.getElementById("product-id").innerHTML;
+  var productName = document.getElementById("product-name").innerHTML;
+  var productPrice = document.getElementById("product-price").innerHTML;
+  var productOridinalPrice = document.getElementById(
+    "product-original-price"
+  ).innerHTML;
+  var productCount = document.getElementById("product-count").innerHTML;
+  var productImage = document
+    .getElementById("product-expanded-image")
+    .getAttribute("src");
+  var itemsContainer = document.getElementById("nav-items");
+  const newItem = document.createElement("div");
+  newItem.classList.add("nav__item");
+  newItem.innerHTML =
+    "<img src=" +
+    productImage +
+    ' alt="image-product" class="nav__cartItemPhoto" /> <div class="nav__cartItemDetails"> <p class="name">' +
+    productName +
+    ' <p class="color">' +
+    productOridinalPrice +
+    '</p> </p><p class="price">' +
+    productPrice +
+    '</p> </div> <div class="nav__cartItemNumberOfOrders"> <button class="nav__deleteItemFromCart"> <img src="imgs/icon-delete.svg" alt="icon-delete" /> </button> <div class="nav__cartItemCounter"> <img src="imgs/icon-minus.svg" alt="icon-minus" /> <p class="nav__numOfItem">' +
+    productCount +
+    '</p> <img src="imgs/icon-plus.svg" alt="icon-plus" /> </div> </div>';
+  itemsContainer.appendChild(newItem);
+  items.push([
+    productID,
+    productName.trim(),
+    productPrice,
+    productOridinalPrice.trim(),
+    productCount,
+    productImage,
+  ]);
+  var itemsConverted = JSON.stringify(items);
+  localStorage.setItem("cart-items", itemsConverted);
 }
